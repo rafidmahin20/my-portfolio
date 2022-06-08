6b34e7569941318e93c08353078e6d5452cc48eb
rafidmahin20 <submanmahin@gmail.com>
2022-06-09 01:02:04 +0600

    edited

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Pages/Header/Header'
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <div>
       <Header></Header>
-      
+      <Routes>
+        <Route path='/'  element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
