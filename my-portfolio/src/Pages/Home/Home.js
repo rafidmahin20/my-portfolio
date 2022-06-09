3ba06eb3c52c93116570c757be3c213e442f825c
rafidmahin20 <submanmahin@gmail.com>
2022-06-09 16:53:37 +0600

    edited

--- my-portfolio/src/Pages/Home/Home.js
+++ my-portfolio/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import Testimonials from './Testimonials';
 
 
 const Home = () => {
     return (
-        <div >
+        <div className='px-12'>
             <Banner></Banner>
+            <Testimonials></Testimonials>
         </div>
     );
 };
